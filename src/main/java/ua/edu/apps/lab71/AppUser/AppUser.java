abf8613fe5f2aefaa88f8c756ec40c92e7300860
maksDev123 <maksym.buleshnyi.pn@ucu.edu.ua>
2023-11-26 20:44:17 +0200

    User

--- src/main/java/ua/edu/apps/lab71/AppUser/AppUser.java
+++ src/main/java/ua/edu/apps/lab71/AppUser/AppUser.java
@@ -1,9 +1,14 @@
 package ua.edu.apps.lab71.AppUser;
 
 import java.time.LocalDate;
+import java.time.Period;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,8 +16,18 @@ import lombok.Setter;
 @Setter @Getter @NoArgsConstructor @Entity
 public class AppUser {
     @Id
-    private int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private int age = 20;
+
+    @Column(unique = true)
+    private String email;
+
     private LocalDate dob;
-    private String email = "";
+
+    @Transient
+    public Period getAge() {
+        return Period.between(dob, LocalDate.now());
+    }
 }
--- src/main/java/ua/edu/apps/lab71/repositories/UserRepository.java
+++ src/main/java/ua/edu/apps/lab71/repositories/UserRepository.java
@@ -1,12 +1,16 @@
 package ua.edu.apps.lab71.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import ua.edu.apps.lab71.AppUser.AppUser;
 
 public interface UserRepository extends CrudRepository<AppUser, Long> {
+    @Query("SELECT u FROM AppUser u WHERE u.email = ?1")
+    Optional<AppUser> findUserByEmail(String email);
 
     List<AppUser> findAll();
     AppUser save(AppUser flower);
